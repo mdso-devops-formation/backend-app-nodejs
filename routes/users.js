f57082c96825906e20b699ad73ac9ed5e4efc9f5
alla ismail <wwwallaismail@gmail.com>
2022-03-30 23:24:22 +0200

    ajout le nom

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let users = [{ "last_name": "Allou", "first_name": "Abdelhafid" }, { "last_name": "Coulibaly", "first_name": "Adama" }]
+    let users = [{ "last_name": "Allou", "first_name": "Abdelhafid" },
+     { "last_name": "Coulibaly", "first_name": "Adama" },
+     { "last_name": "alla", "first_name": "ismail" }
+    ]
     res.send(users);
 });
 
