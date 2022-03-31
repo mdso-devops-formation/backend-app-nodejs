dbde6f889d8da0fab58e47e1a8487843d475f2d0
mouhsineguet <96666995+mouhsineguet@users.noreply.github.com>
2022-03-31 21:23:21 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     let users = [{ "last_name": "Allou", "first_name": "Abdelhafid" },
      { "last_name": "Coulibaly", "first_name": "Adama" },
-     { "last_name": "alla", "first_name": "ismail" }
+     { "last_name": "alla", "first_name": "ismail" },
+     { "last_name": "Mouhsine", "first_name": "GUETTI" }
     ]
     res.send(users);
 });
