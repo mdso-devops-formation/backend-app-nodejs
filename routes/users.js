9160a9331be7cd6f99a8309ac8bb796aa9efd4b2
farah hajji <farahhajji55@gmail.com>
2022-04-03 23:24:33 +0000

    5 commit

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     let users = [{ "last_name": "Allou", "first_name": "Abdelhafid" },
-     { "last_name": "Coulibaly", "first_name": "Adama" },
-     { "last_name": "alla", "first_name": "ismail" },
-     { "last_name": "Mouhsine", "first_name": "GUETTI" },
-     { "last_name": "Abdelhak", "first_name": "Elgourmat" }
+        { "last_name": "Coulibaly", "first_name": "Adama" },
+        { "last_name": "alla", "first_name": "ismail" },
+        { "last_name": "Mouhsine", "first_name": "GUETTI" },
+        { "last_name": "Abdelhak", "first_name": "Elgourmat" },
+        { "last_name": "Hajji", "first_name": "Farah" }
     ]
     res.send(users);
 });
