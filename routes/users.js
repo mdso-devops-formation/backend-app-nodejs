82161beac8716e1cbed4a4b4a534aaba1dc66be3
abdelhak elgourmat <andelhakabdelhak200@gmail.com>
2022-04-03 22:21:42 +0000

    abdelhak

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     let users = [{ "last_name": "Allou", "first_name": "Abdelhafid" },
      { "last_name": "Coulibaly", "first_name": "Adama" },
      { "last_name": "alla", "first_name": "ismail" },
-     { "last_name": "Mouhsine", "first_name": "GUETTI" }
+     { "last_name": "Mouhsine", "first_name": "GUETTI" },
      { "last_name": "Abdelhak", "first_name": "Elgourat" }
     ]
     res.send(users);
